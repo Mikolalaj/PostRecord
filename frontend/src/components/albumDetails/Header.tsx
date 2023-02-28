fef1acd913d0fa0b4af5c35e190cd7bdafb6b765
Mikolaj Olejnik <mikolaj.olejnik@marklogic.com>
2023-02-28 23:35:32 +0100

    moved Header to seperate component

---
+++ frontend/src/components/albumDetails/Header.tsx
@@ -0,0 +1,58 @@
+import { Button, Flex, Group, Image, Stack, Text, Title } from '@mantine/core'
+import { IconHeartPlus, IconPlus } from '@tabler/icons-react'
+import { ArtistType } from '../../types'
+
+interface HeaderProps {
+    title: string
+    image: string
+    artist: ArtistType
+    genre: string
+    style: string
+    year: string
+    spotifyId: string
+}
+
+function Header({ title, image, artist, genre, style, year, spotifyId }: HeaderProps) {
+    return (
+        <Flex gap='lg' align='flex-start'>
+            <Image src={image} alt={title} width={250} height={250} />
+            <Stack align='flex-start' justify='space-between'>
+                <Title>
+                    <Text component='span' inherit color='violet' style={{ fontSize: 40 }}>
+                        {title}
+                    </Text>{' '}
+                    <Text component='span' weight={200} size='xl'>
+                        by {artist.name}
+                    </Text>
+                </Title>
+                <Group style={{ width: '100%' }}>
+                    <Flex direction='column' justify='center' align='flex-start'>
+                        <Text weight={500}>Label: </Text>
+                        <Text weight={500}>Genre: </Text>
+                        <Text weight={500}>Style: </Text>
+                        <Text weight={500}>Year: </Text>
+                    </Flex>
+                    <Flex direction='column' justify='center' align='flex-start'>
+                        <Text>Capitol Records</Text>
+                        <Text>{genre}</Text>
+                        <Text>{style}</Text>
+                        <Text>{year}</Text>
+                    </Flex>
+                </Group>
+                <Group mt='xl'>
+                    <Button variant='light' leftIcon={<IconPlus size={20} />}>
+                        Add to collection
+                    </Button>
+                    <Button color='pink' variant='outline' leftIcon={<IconHeartPlus size={20} />}>
+                        Set as favorite
+                    </Button>
+                    <Button color='green.6' component='a' target='_blank' href={`https://open.spotify.com/album/${spotifyId}`}>
+                        Listen on Spotify
+                    </Button>
+                </Group>
+            </Stack>
+        </Flex>
+    )
+}
+
+export default Header
--- frontend/src/pages/Album.tsx
+++ frontend/src/pages/Album.tsx
@@ -1,6 +1,6 @@
-import { Button, Flex, Grid, Group, Image, Stack, Text, Title } from '@mantine/core'
-import { IconHeartPlus, IconPlus } from '@tabler/icons-react'
+import { Flex, Grid } from '@mantine/core'
 import AboutArtist from '../components/albumDetails/AboutArtist'
+import Header from '../components/albumDetails/Header'
 import TrackList from '../components/albumDetails/TrackList'
 import VinylPressings from '../components/albumDetails/VinylPressings'
 import { AlbumType } from '../types'
@@ -133,47 +133,10 @@ const albumData: AlbumType = {
 
 function Album() {
     // function Album({ title, artist, image, pressings }: AlbumProps) {
-    const { title, artist, image, pressings, spotifyId, tracklist } = albumData
+    const { artist, pressings, tracklist } = albumData
     return (
         <Flex gap='xl' direction='column'>
-            <Flex gap='lg' align='flex-start'>
-                <Image src={image} alt={title} width={250} height={250} />
-                <Stack align='flex-start' justify='space-between'>
-                    <Title>
-                        <Text component='span' inherit color='violet' style={{ fontSize: 40 }}>
-                            {title}
-                        </Text>{' '}
-                        <Text component='span' weight={200} size='xl'>
-                            by {artist.name}
-                        </Text>
-                    </Title>
-                    <Group style={{ width: '100%' }}>
-                        <Flex direction='column' justify='center' align='flex-start'>
-                            <Text weight={500}>Label: </Text>
-                            <Text weight={500}>Genre: </Text>
-                            <Text weight={500}>Style: </Text>
-                            <Text weight={500}>Year: </Text>
-                        </Flex>
-                        <Flex direction='column' justify='center' align='flex-start'>
-                            <Text>Capitol Records</Text>
-                            <Text>{albumData.genre}</Text>
-                            <Text>{albumData.style}</Text>
-                            <Text>{albumData.year}</Text>
-                        </Flex>
-                    </Group>
-                    <Group mt='xl'>
-                        <Button variant='light' leftIcon={<IconPlus size={20} />}>
-                            Add to collection
-                        </Button>
-                        <Button color='pink' variant='outline' leftIcon={<IconHeartPlus size={20} />}>
-                            Set as favorite
-                        </Button>
-                        <Button color='green.6' component='a' target='_blank' href={`https://open.spotify.com/album/${spotifyId}`}>
-                            Listen on Spotify
-                        </Button>
-                    </Group>
-                </Stack>
-            </Flex>
+            <Header {...albumData} />
             <Grid gutter={50}>
                 <Grid.Col lg={6} sm={6} xs={12}>
                     <TrackList tracklist={tracklist} />
