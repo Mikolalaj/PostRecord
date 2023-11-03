7281266900151d5fe36b238145a05f48a0c17105
Mikolaj Olejnik <olejnikmikolaj@gmail.com>
2023-11-04 00:29:47 +0100

    added azure storage blob

---
+++ backend/src/azure/image.ts
@@ -0,0 +1,21 @@
+import { BlockBlobClient, BlobServiceClient, StorageSharedKeyCredential } from '@azure/storage-blob'
+
+const accountName = process.env.AZURE_STORAGE_NAME
+const accountKey = process.env.AZURE_STORAGE_KEY
+if (!accountName) throw Error('Azure Storage account name not found')
+if (!accountKey) throw Error('Azure Storage account key not found')
+
+const sharedKeyCredential = new StorageSharedKeyCredential(accountName, accountKey)
+
+const baseUrl = `https://${accountName}.blob.core.windows.net`
+const containerName = `records`
+
+const blobServiceClient = new BlobServiceClient(`${baseUrl}`, sharedKeyCredential)
+const containerClient = blobServiceClient.getContainerClient(containerName)
+
+export async function uploadBlobFromBuffer(blobName: string, buffer: Buffer): Promise<void> {
+    const blockBlobClient: BlockBlobClient = containerClient.getBlockBlobClient(blobName)
+
+    // Upload buffer
+    await blockBlobClient.uploadData(buffer)
+}
